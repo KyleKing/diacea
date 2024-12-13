42498285c01248b0c0433579c5068b8bca1b9c43
Kyle King <KyleKing@users.noreply.github.com>
2024-12-12 20:54:10 -0500

    feat: initialize the project

---
+++ README.md
@@ -0,0 +1,5 @@
+# diacea
+
+TBD
+
+Documentation can be found on [GitHub (./docs)](./docs), [PyPi](https://pypi.org/project/diacea/), or [Hosted](https://diacea.kyleking.me/)!
---
+++ diacea/__init__.py
@@ -0,0 +1,11 @@
+"""diacea."""
+
+from ._runtime_type_check_setup import configure_runtime_type_checking_mode
+
+__version__ = '0.0.1'
+__pkg_name__ = 'diacea'
+
+configure_runtime_type_checking_mode()
+
+
+# == Above code must always be first ==
